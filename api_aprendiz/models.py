596a23c6b4188db5a162c8325a4cfcd00b28a183
Edwin Chilatra <efchilatra@gmail.com>
2023-08-30 10:34:19 -0500

    Creacion Api_Aprendices

--- api_aprendiz/models.py
+++ api_aprendiz/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Aprendiz(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    numero_documento = models.CharField(max_length=20)
+    tipo_documento = models.CharField(max_length=4)
+    numero_ficha =models.PositiveIntegerField()
